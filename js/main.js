368d7288daf8bc1a9e4f5afc87a139dcf5a53d74
oskar shtefan <oskar.shtefan@gmail.com>
2022-03-07 17:05:37 +0600

    added intro section with swiper

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".mySwiper", {
+    loop:true,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
